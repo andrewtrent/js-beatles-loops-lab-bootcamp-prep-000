373a1a6e91990d7e95dfc4357af9f7316bae5c7a
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-17 01:47:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function theBeatlesPlay(musicians,instruments){
 
 function johnLennonFacts(facts){
   var i=0
+  var exclaim = []
   while (i<facts.length){
-    
+  exclaim.push(`{$facts[i]}!!!`)
   }
 }
